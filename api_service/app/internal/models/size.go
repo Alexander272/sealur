f8f7205b1d48db0a101dc2c57cb90450bf8c3961
Alexander272 <martynovalexander272@gmail.com>
2022-04-25 16:25:17 +0500

    changed sizes

--- api_service/app/internal/models/size.go
+++ api_service/app/internal/models/size.go
@@ -8,18 +8,18 @@ type GetSizesDTO struct {
 }
 
 type SizesDTO struct {
-	Flange   string  `json:"flange" binding:"required"`
-	TypeFlId string  `json:"typeFlId" binding:"required"`
-	Dn       string  `json:"dn" binding:"required"`
-	Pn       string  `json:"pn" binding:"required"`
-	TypePr   string  `json:"typePr" binding:"required"`
-	StandId  string  `json:"standId"`
-	D4       float32 `json:"d4"`
-	D3       float32 `json:"d3" binding:"required"`
-	D2       float32 `json:"d2" binding:"required"`
-	D1       float32 `json:"d1"`
-	H        string  `json:"h" binding:"required"`
-	S2       string  `json:"s2"`
-	S3       string  `json:"s3"`
-	Number   int32   `json:"number"`
+	Flange   string `json:"flange" binding:"required"`
+	TypeFlId string `json:"typeFlId" binding:"required"`
+	Dn       string `json:"dn" binding:"required"`
+	Pn       string `json:"pn" binding:"required"`
+	TypePr   string `json:"typePr" binding:"required"`
+	StandId  string `json:"standId"`
+	D4       string `json:"d4"`
+	D3       string `json:"d3" binding:"required"`
+	D2       string `json:"d2" binding:"required"`
+	D1       string `json:"d1"`
+	H        string `json:"h" binding:"required"`
+	S2       string `json:"s2"`
+	S3       string `json:"s3"`
+	Number   int32  `json:"number"`
 }
--- pro_service/app/internal/models/sizes.go
+++ pro_service/app/internal/models/sizes.go
@@ -1,16 +1,16 @@
 package models
 
 type Size struct {
-	Id       string  `db:"id"`
-	Dn       string  `db:"dn"`
-	Pn       string  `db:"pn"`
-	D4       float32 `db:"d4"`
-	D3       float32 `db:"d3"`
-	D2       float32 `db:"d2"`
-	D1       float32 `db:"d1"`
-	H        string  `db:"h"`
-	S2       string  `db:"s2"`
-	S3       string  `db:"s3"`
-	TypePr   string  `db:"type_pr"`
-	TypeFlId string  `db:"type_fl_id"`
+	Id       string `db:"id"`
+	Dn       string `db:"dn"`
+	Pn       string `db:"pn"`
+	D4       string `db:"d4"`
+	D3       string `db:"d3"`
+	D2       string `db:"d2"`
+	D1       string `db:"d1"`
+	H        string `db:"h"`
+	S2       string `db:"s2"`
+	S3       string `db:"s3"`
+	TypePr   string `db:"type_pr"`
+	TypeFlId string `db:"type_fl_id"`
 }
